c91302d4bf3a3953f4e02bfeaf297373df8ad881
Artem <45859415+47294@users.noreply.github.com>
2021-11-27 13:40:01 +0300

    Update Head.h

--- includes/Head.h
+++ includes/Head.h
@@ -1,3 +1,12 @@
+#define CITY_LENGTH 30
+#define STREET_LENGTH 50
+typedef struct _Address
+{
+  char City[CITY_LENGTH];
+  char Street[STREET_LENGTH];
+  int BuildingNo;
+} Address;
+
 bool ReadTextAddress(const char*, Address*);
 bool WriteTextAddress(const char*, const Address*);
 bool ReadBinaryAddress(const char*, Address*);
