024f2feec00bb628bfdc43055ef02c534674b2f6
Artem <45859415+47294@users.noreply.github.com>
2021-11-27 13:26:56 +0300

    Add files via upload

---
+++ includes/head.h
@@ -0,0 +1,4 @@
+bool ReadTextAddress(const char*, Address*)
+bool WriteTextAddress(const char*, const Address*);
+bool ReadBinaryAddress(const char*, Address*);
+bool WriteBinaryAddress(const char*, const Address*);
